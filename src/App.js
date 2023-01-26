085a896eef3649efd3896a321b89cc85bc241347
Chinwenma <okoriechinwe1327@gmail.com>
2023-01-26 12:25:09 -0800

    img

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Info from './components/Info';
 import Interest from './components/Interest';
+import images from'./images/profile.jpeg';
 
 function App() {
   return (
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,8 @@
+import profile from '../images/profile.jpeg';
 export default function Info() {
     return (
         <div className="flex flex-center justify-between items-center">
-            <div> <img src="../images/profile.jpeg" alt="" className="h-10 w-10" /> </div>
+            <div> <img src={profile} alt="" /> </div>
 
             <h2 className="max-w-md text-2xl font-normal  text-center text-white md:text-5xl md:text-left">Okorie Chinwenma</h2>
         </div>
