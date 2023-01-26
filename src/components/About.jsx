ad2f1c359559e8e24081187201342006922d855c
Chinwenma <okoriechinwe1327@gmail.com>
2023-01-26 01:38:38 -0800

    myreactbusinesscard

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+       <div>
+        <h2 className="text-4xl text-start text-slate-700">About</h2>
+        <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus labore quasi culpa, maiores dignissimos excepturi eaque ratione quas, dicta quia veritatis voluptatibus eveniet repellendus, quo eius iste omnis! Exercitationem, cupiditate.</p>
+       </div>
+    )
+}
---
+++ src/components/Info.jsx
@@ -0,0 +1,7 @@
+export default function Info(){
+    return(
+        <div className="flex flex-center justify-center items-center">
+            <img src="../images/profile.jpeg" alt="" className="h-10 w-10" />
+        </div>
+    )
+}
